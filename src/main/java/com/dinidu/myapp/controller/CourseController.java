1d8f912a6122659603bc8be84d66d8c2ae259fab
Dinidu21 <dinidusachintha3@gmail.com>
2025-02-26 22:26:41 +0530

    code:CURD

--- src/main/java/com/dinidu/myapp/controller/CourseController.java
+++ src/main/java/com/dinidu/myapp/controller/CourseController.java
@@ -4,6 +4,8 @@ import com.dinidu.myapp.model.entity.CourseDetails;
 import com.dinidu.myapp.service.CourseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -15,28 +17,28 @@ public class CourseController {
         this.courseService = courseService;
     }
 
-    // Dashboard - List all courses
     @GetMapping
     public String getDashboard(Model model) {
         model.addAttribute("courses", courseService.getAllCourses());
         return "dashboard";
     }
 
-    // Show add course form
     @GetMapping("/add")
     public String showAddCourseForm(Model model) {
-        model.addAttribute("courses", new CourseDetails());
+        model.addAttribute("course", new CourseDetails());
         return "add-course";
     }
 
-    // Save new course
     @PostMapping("/save")
-    public String saveCourse(@ModelAttribute("courses") CourseDetails course) {
+    public String saveCourse(@Validated @ModelAttribute("course") CourseDetails course,
+                             BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "add-course"; // Return to form with errors
+        }
         courseService.saveCourse(course);
         return "redirect:/courses";
     }
 
-    // Show edit course form
     @GetMapping("/edit/{courseCode}")
     public String showEditCourseForm(@PathVariable String courseCode, Model model) {
         CourseDetails course = courseService.getCourseByCode(courseCode);
@@ -44,14 +46,17 @@ public class CourseController {
         return "edit-course";
     }
 
-    // Update course
     @PostMapping("/update/{courseCode}")
-    public String updateCourse(@PathVariable String courseCode, @ModelAttribute("course") CourseDetails course) {
+    public String updateCourse(@PathVariable String courseCode,
+                               @Validated @ModelAttribute("course") CourseDetails course,
+                               BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "edit-course"; // Return to form with errors
+        }
         courseService.updateCourse(courseCode, course);
         return "redirect:/courses";
     }
 
-    // Delete course
     @GetMapping("/delete/{courseCode}")
     public String deleteCourse(@PathVariable String courseCode) {
         courseService.deleteCourse(courseCode);
--- src/main/java/com/dinidu/myapp/model/entity/CourseDetails.java
+++ src/main/java/com/dinidu/myapp/model/entity/CourseDetails.java
@@ -6,20 +6,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CourseDetails {
     @Id
     private String courseCode;
     private String courseName;
     private float labCount;
     private int lecHours;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
 }
